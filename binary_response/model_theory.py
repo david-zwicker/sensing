e478049b50c381ec8590bc37db54ee09cc7e3c6e
David Zwicker <dzwicker@seas.harvard.edu>
2015-04-17 09:38:38 -0400

    Added new mutual information estimates for the homogeneous case

--- binary_response/model_theory.py
+++ binary_response/model_theory.py
@@ -58,17 +58,31 @@ class ReceptorLibraryTheory(ReceptorLibraryBase):
         return 1 - np.prod(1 - self.frac*self.substrate_probability)
 
 
-    def mutual_information(self):
+    def mutual_information(self, with_correlations=True, approx_prob=False):
         """ return a theoretical estimate of the mutual information between
         input and output """
-        assert len(np.unique(self._hs)) == 1
+        assert len(np.unique(self.commonness)) == 1
+        p_s = self.substrate_probability
         
-        # probability that a single receptor is activated
-        p1 = self.activity_single()
+        # probability that a single receptor and a pair is activated 
+        if approx_prob:
+            # use approximate formulas for calculating the probabilities
+            p1 = self.Ns * self.frac * p_s
+            p2 = self.Ns * self.frac**2 * p_s
+        else:
+            # use better formulas for calculating the probabilities 
+            p1 = self.activity_single()
+            p2 = 1 - (1 - self.frac**2 * p_s)**self.Ns
         
         if p1 == 0:
+            # receptors are never activated
             return 0
+        elif with_correlations:
+            # use the estimated formula that includes the effects of correlations
+            corr = self.Nr*(self.Nr - 1)*(1 - 2*p1 + 0.75*p2)*p2
+            return self.Nr - 0.5*self.Nr*(1 - 2*p1)**2 - corr
         else:
+            # use the simple formula where receptors are considered independent
             return -self.Nr*(p1*np.log2(p1) + (1 - p1)*np.log2(1 - p1))
         
         
@@ -90,7 +104,7 @@ class ReceptorLibraryTheory(ReceptorLibraryBase):
              
         else:
             # check whether the mixtures are all homogeneous
-            assert len(np.unique(self._hs)) == 1
+            assert len(np.unique(self.commonness)) == 1
             p0 = self.substrate_probability[0]
             
         # calculate the fraction for the homogeneous case
