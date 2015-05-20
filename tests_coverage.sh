e9f01b24afad0e51bb44962f5da6282ed120bc17
David Zwicker <dzwicker@seas.harvard.edu>
2015-05-20 11:46:47 -0400

    Ignore docs/coverage directoty in git, but create it when necessary

--- tests_coverage.sh
+++ tests_coverage.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo 'Determine coverage of all unittests...'
+
+mkdir -p docs/coverage
+
 nosetests-2.7 --with-coverage \
     --cover-erase --cover-inclusive \
     --cover-package=binary_response \
