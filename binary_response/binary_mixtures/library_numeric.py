0215a6159b46bdd3a80659f971a53d1bcd6d4235
David Zwicker <dzwicker@seas.harvard.edu>
2015-08-24 14:27:45 -0400

    Updated the way in which error of optimization is estimated

--- binary_response/binary_mixtures/library_numeric.py
+++ binary_response/binary_mixtures/library_numeric.py
@@ -1081,7 +1081,9 @@ class LibraryBinaryNumeric(LibraryBinaryBase):
         self.int_mat = state.copy()
 
         if ret_error:
-            return (result_best[0], np.std(values)), result_best[1:]
+            value_best = result_best[0]
+            value_err = np.abs(value_best - np.median(values))
+            return (value_best, value_err), result_best[1:]
         else:
             return result_best
                                
