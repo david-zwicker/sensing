622e261bf66a0edb990ab8de57bb5633958a8177
David Zwicker <dzwicker@seas.harvard.edu>
2015-09-18 14:14:40 -0400

    Fixed bug with wrong import

--- binary_response/gaussian_mixtures/__init__.py
+++ binary_response/gaussian_mixtures/__init__.py
@@ -1,4 +1,4 @@
 from .lib_gau_numeric import LibraryGaussianNumeric
 #from .lib_gau_theory import LibraryGaussianTheory
 
-__all__ = ['LibraryGaussianNumeric', 'LibraryGaussianTheory']
+__all__ = ['LibraryGaussianNumeric']
