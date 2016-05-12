Added routine to calculate the sensitivity statistics Fixed bug in the calculation of the Pearson correlation, which would sometime return nan

binary_response/library_numeric_base.py
@@ -319,10 +319,10 @@ class LibraryNumericMixin(object):
             result['pearson'] = corr
             ret.remove('pearson')
         if 'pearson_mean' in ret:
-            result['pearson_mean'] = corr_tri.mean()
+            result['pearson_mean'] = np.nanmean(corr_tri)
             ret.remove('pearson_mean')
         if 'pearson_std' in ret:
-            result['pearson_std'] = corr_tri.std()
+            result['pearson_std'] = np.nanstd(corr_tri)
             ret.remove('pearson_std')
             
         if ret:

binary_response/sparse_mixtures/lib_spr_numeric.py
@@ -66,7 +66,7 @@ class LibrarySparseNumeric(LibraryNumericMixin, LibrarySparseBase):
 
     def choose_sensitivity_matrix(self, distribution, mean_sensitivity=1,
                                   **kwargs):
-        """ chooses the sensitivity matrix """
+        """ choose the sensitivity matrix. Docstring is overwritten below """
         self.sens_mat, sens_mat_params = get_sensitivity_matrix(
                   self.Nr, self.Ns, distribution, mean_sensitivity, **kwargs)
 
@@ -74,6 +74,15 @@ class LibrarySparseNumeric(LibraryNumericMixin, LibrarySparseBase):
         self.parameters['sensitivity_matrix_params'] = sens_mat_params
 
     choose_sensitivity_matrix.__doc__ = get_sensitivity_matrix.__doc__  
+
+
+    def sensitivities_statistics(self):
+        """ returns statistics of the sensitivity matrix """
+        Sni_var = self.sens_mat.var()
+        
+        # return the results in a dictionary to be able to extend it later
+        return {'mean': self.sens_mat.mean(), 'std': np.sqrt(Sni_var),
+                'var': Sni_var}
 
 
     @property